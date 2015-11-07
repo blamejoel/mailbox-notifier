0cf7e6973eb6dd472bee5182711263daa6621445
Joel Gomez <joel.gomez@email.ucr.edu>
2015-11-07 13:18:02 -0800

    send function sends

--- src/tx.cpp
+++ src/tx.cpp
@@ -1,5 +1,8 @@
 #include <stdlib.h>
 #include <stdio.h>
+#include <string>
+#include <iomanip>
+#include <iostream>
 
 #ifdef _WIN32
 #include <Windows.h>
@@ -9,37 +12,69 @@
 
 #include "rs-232/rs232.h"
 
-int main() {
-    int i = 0, 
-        cport_nr = 0, 
-        bdrate = 9600;
+#define COM     24          //comport 24=ttyACM0
+#define BAUD    9600
 
-    char mode[]={'8', 'N', '1', 0},
-        str[2][512];
+using namespace std;
 
-    strcpy(str[0], "test\n");
-    strcpy(str[1], "testing\n");
+int main(int argc, char* argv[]) {
+    /* int i = 0, */ 
+    int cport_nr = COM, 
+        bdrate = BAUD;
+
+    char mode[]={'8', 'N', '1', 0};
 
     if (RS232_OpenComport(cport_nr, bdrate, mode)) {
-        printf("Unable to open comport\n");
-
+        //exit if failed to open serial port!
+        printf("Arduino not detected! Aborting.\n");
         return 0;
     }
 
-    while(1) {
-        RS232_cputs(cport_nr, str[i]);
-        printf("sent: %s\n", str[i]);
-
-#ifdef _WIN32
-    Sleep(100);
-#else
-    usleep(100000);
-#endif
-
-        i++;
-        i %= 2;
-
+    int command;
+    if (strcmp(argv[1], "30") == 0) {
+        command = 48;
     }
+    else if (strcmp(argv[1], "31") == 0) {
+        command = 49;
+    }
+    else {
+        command = 0;
+    }
+
+    int error;
+    if (command != 0) {
+        error = RS232_SendByte(COM, (char)command);
+        if (error) {
+            printf("%s %s", "Unable to send", argv[1]);
+        }
+    }
+    string result;
+    if (command == 48) {
+        result = "Monitoring Enabled!";
+    }
+    else if (command == 49) {
+        result = "Monitoring Disabled!";
+    }
+    else {
+        result = "Invalid command!";
+    }
+    printf("%s\n", result.c_str());
+
+// manual entry
+    /* while(1) { */
+    /*     RS232_cputs(cport_nr, str[i]); */
+    /*     printf("sent: %s\n", str[i]); */
+
+/* #ifdef _WIN32 */
+    /* Sleep(100); */
+/* #else */
+    /* usleep(100000); */
+/* #endif */
+
+    /*     i++; */
+    /*     i %= 2; */
+
+    /* } */
 
 return 0;
 }
