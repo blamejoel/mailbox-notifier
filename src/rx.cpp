987d5f31d271e13543fa9912571899a0f31f4040
Joel Gomez <joel.gomez@email.ucr.edu>
2015-11-07 19:07:20 -0800

    fixed date function, added test without com port

--- src/rx.cpp
+++ src/rx.cpp
@@ -15,20 +15,53 @@ using namespace std;
 
 ofstream outputFile;
 
-int main() {
+string space_replace(string &text) {
+    int size = text.size();
+    for (int i = 0; i < size; i++) {
+        if (text[i] == ' ') {
+            text[i] = '_';
+        }
+    }
+    return text;
+}
+
+string exec(const char* cmd) {
+    FILE* pipe = popen(cmd, "r");
+    if (!pipe) return "ERROR";
+    char buffer[128];
+    string result = "";
+    while (!feof(pipe)) {
+        if (fgets(buffer, 128, pipe) != NULL)
+            result += buffer;
+    }
+    pclose(pipe);
+    return result;
+}
+
+int main(int argc, char* argv[]) {
     int i, n,
         cport_nr=24,
         bdrate=9600;
 
     unsigned char buf[4096];
-    unsigned char CONFIRM = 0xEF;
+    /* unsigned char CONFIRM = 0xEF; */
 	
     char mode[]={'8', 'N', '1', 0};
 
-    outputFile.open("log");
+    if (argc > 1) {
+        string log = "log/";
+        string s = exec("date");
+        log += s;
+        log.erase(log.size() - 1);
+        space_replace(log);
+        outputFile.open(log.c_str());
+        printf("You have mail!\n");
+        outputFile << "Package received!" << endl;
+        outputFile << s << endl;
+    }
 
     if (RS232_OpenComport(cport_nr, bdrate, mode)) {
-        printf("Unable to open comport\n");
+        printf("Unable to find Arduino.\n");
 
         return 0;
     }
@@ -44,31 +77,23 @@ int main() {
                     buf[i] = '.';
                 }
             }
-            char test = buf[0];
-            if (test == 16) {
-                printf("package received!");
-				outputFile << "Package received!" << endl;
-                RS232_SendByte(cport_nr, CONFIRM);
-            }
-            /*else if (test == 0x1A) {
-                printf("package stolen!");
-                RS232_SendByte(cport_nr, CONFIRM);
-            }
-             printf("You have mail!"); */
-            /* printf("received %i bytes: %s\n", n, (char*)buf); */
+
+            string log = "log/";
+            string s = exec("date");
+            log += s;
+            log.erase(log.size() - 1);
+            space_replace(log);
+            outputFile.open(log.c_str());
             printf("You have mail!\n");
-			outputFile << "Package received!" << endl;
+            outputFile << "Package received!" << endl;
+			outputFile << s << endl;
         }
 
 
-time_t timev;
 #ifdef _WIN32
     Sleep(100);
-	outputFile << "Date: " << time(&timev) << endl;	//output date to the log file
 #else
     usleep(100000);
-	outputFile << "Date: " << time(&timev) << endl; //output date to the log file
-
 #endif
 
     outputFile.close();
